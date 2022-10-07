42231e88e7464583ed06c72d29d722b03fe9b240
Sparsh <sparsh.chadha46@gmail.com>
2022-10-07 23:36:38 +0530

    corrected progress bar visibility

--- app/src/main/java/com/example/ping/views/activity/MainActivity.kt
+++ app/src/main/java/com/example/ping/views/activity/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.example.ping.R
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.*
@@ -102,5 +103,10 @@ class MainActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mProgressBar.isVisible = false
     }
 }
