6f9d009b622cdfdd8b5fecee1af8ed2acd4fa334
Sparsh <sparsh.chadha46@gmail.com>
2022-10-29 23:48:14 +0530

    added contact list

--- app/src/main/java/com/example/ping/views/activity/UsersActivity.kt
+++ app/src/main/java/com/example/ping/views/activity/UsersActivity.kt
@@ -1,8 +1,14 @@
 package com.example.ping.views.activity
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -13,6 +19,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ping.R
+import com.example.ping.model.ContactModel
 import com.example.ping.model.User
 import com.example.ping.views.adapter.UsersAdapter
 import com.google.firebase.auth.FirebaseAuth
@@ -29,6 +36,8 @@ class UsersActivity : AppCompatActivity() {
     private lateinit var adapter: UsersAdapter
     private lateinit var dbRef: DatabaseReference
     private lateinit var progressBar: ProgressBar
+    private lateinit var contactsList : HashSet<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,30 +55,98 @@ class UsersActivity : AppCompatActivity() {
         usersRecyclerView.adapter = adapter
 
 
-//        Loading Users from database -
-        loadUsers()
+//        Retrieving contacts -
+        showContacts()
+ //        CONTACT LIST RETRIEVED BUT OPERATIONS HAVE BECOME SLOWER(LIKE CRUISING THROUGH ACTIVITIES
+
+    }
+
+    private fun showContacts() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), 10)
+        } else {
+            // If android version < 6, permission to read contacts is not required
+            contactsList = getContactsList()
+        }
+    }
+
+    @SuppressLint("Recycle", "Range")
+    private fun getContactsList() : HashSet<String>{
+        contactsList = hashSetOf()
+        val uri = ContactsContract.Contacts.CONTENT_URI
+        val sort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+
+        val cursor = contentResolver.query(uri, null, null, null, sort)
+
+        if (cursor!!.count > 0){
+            while (cursor.moveToNext()){
+                val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val uriPhone = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                val selection = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?"
+                val phoneCursor = contentResolver.query(uriPhone, null, selection, arrayOf(id), null)
+
+                if (phoneCursor!!.moveToNext()){
+                    val number = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+//                    val contact = ContactModel(name, number)
+                    contactsList.add(removeSpaces(number))
+                    phoneCursor.close()
+                }
+            }
+        }
+
+ //        Loading Users from database -
+        loadUsers(contactsList)
+        cursor.close()
+        return contactsList
+    }
+
+    private fun removeSpaces(number: String) : String{
+        var numberWithoutSpaces = ""
+        for (i in number.indices){
+            if(number[i] == ' '){
+                continue
+            }
+            numberWithoutSpaces += number[i]
+        }
+
+        return numberWithoutSpaces
     }
 
 
-    private fun loadUsers() = CoroutineScope(Dispatchers.IO).launch {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 10) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                showContacts()
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we cannot display users.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun loadUsers(contacts: HashSet<String>) = CoroutineScope(Dispatchers.IO).launch {
         try {
+//            contactsList = hashSetOf()
             dbRef.child("user").addValueEventListener(object : ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
                     usersList.clear()
                     for (item in snapshot.children){
                         val currentUser = item.getValue(User::class.java)
-                        if (auth.currentUser?.uid != currentUser?.uid){
-                            usersList.add(currentUser!!)
+                        if (auth.currentUser?.uid != currentUser?.uid && contacts.contains(currentUser!!.number)){
+                            usersList.add(currentUser)
                         }
                     }
-
+                    Log.i("Contacts List ->", contacts.toString())
                     progressBar.visibility = View.GONE
                     usersRecyclerView.visibility = View.VISIBLE
                     adapter.notifyDataSetChanged()
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
+
                 }
             })
         } catch (e: Exception){
@@ -130,7 +207,7 @@ class UsersActivity : AppCompatActivity() {
                 usersList.clear()
                 for (item in snapshot.children){
                     val currentUser = item.getValue(User::class.java)
-                    if (auth.currentUser?.uid != currentUser?.uid && currentUser?.name?.lowercase()!!.contains(initials!!.lowercase())){
+                    if (auth.currentUser?.uid != currentUser?.uid && currentUser?.name?.lowercase()!!.contains(initials!!.lowercase()) && contactsList.contains(currentUser.number)){
                         usersList.add(currentUser)
                     }
                 }
