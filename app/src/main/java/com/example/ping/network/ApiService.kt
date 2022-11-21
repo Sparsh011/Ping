47149a32c2e85e599fe84d8ba9875a3d0dd296cd
Sparsh <sparsh.chadha46@gmail.com>
2022-11-21 23:54:10 +0530

    meme fetch speed increased

--- app/src/main/java/com/example/ping/network/ApiService.kt
+++ app/src/main/java/com/example/ping/network/ApiService.kt
@@ -1,13 +1,47 @@
 package com.example.ping.network
 
 import com.example.ping.utils.Constants
+import okhttp3.Cache
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.dnsoverhttps.DnsOverHttps
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.net.InetAddress
+import java.net.Proxy
+import java.util.concurrent.TimeUnit
 
 object ApiService {
+
+    private val appCache = Cache(File("cacheDir", "okhttpcache"), 10 * 1024 * 1024)
+
+    private val bootstrapClient = OkHttpClient.Builder()
+        .cache(appCache)
+        .proxy(Proxy.NO_PROXY)
+        .build()
+
+    private val dns = DnsOverHttps.Builder()
+        .client(bootstrapClient)
+        .url("https://dns.google/dns-query".toHttpUrl())
+        .bootstrapDnsHosts(InetAddress.getByName("8.8.4.4"), InetAddress.getByName("8.8.8.8"))
+        .build()
+
+    private val okHttpClient = OkHttpClient().newBuilder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .dns(dns)
+        .proxy(Proxy.NO_PROXY)
+        .build()
+
+
     val api : ApiInterface by lazy {
         Retrofit.Builder().baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(ApiInterface::class.java)
     }
